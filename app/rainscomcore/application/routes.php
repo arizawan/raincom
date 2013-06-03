0e81a22b18129a261e8b123cf49c4761a19e5401
arizawan <ahm.rizawan@gmail.com>
2013-06-03 19:06:10 +0600

    Dev

--- app/rainscomcore/application/routes.php
+++ app/rainscomcore/application/routes.php
@@ -14,13 +14,56 @@ Route::get('/', function()
 });
 
 # Admin login Route
-Route::get('/admin/login', 'oscore@login');
+Route::get('/admin/login', array('before' => 'alreadycoreloggedin', 'uses' => 'oscore@login'));
 Route::get('/admin/logout', 'oscore@logout');
 Route::post('/admin/loginuser', 'oscore@loginuser');
 Route::post('/admin/resetpass', 'oscore@resetpass');
 Route::post('/admin/register', 'oscore@register');
-Route::get('/admin', 'oscore@index');
 Route::get('/activateadmin/(:any)/(:any)', 'oscore@activatecoreuser');
+Route::get('/admin', array('before' => 'corelogin', 'uses' => 'oscore@index'));
+
+Route::get('sendmail', function(){
+
+	/*
+    // Get the Swift Mailer instance
+	$mailer = IoC::resolve('mailer');
+	$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')->setUsername('ahm.rizawan@gmail.com')->setPassword('strongp455w0rd');
+	$message = Swift_Message::newInstance($transport);
+
+	// Construct the message
+	$message = Swift_Message::newInstance('Message From Website')
+		->attach(Swift_Attachment::fromPath(path('public').'favicon.ico'))
+	    ->setFrom(array('ratuladm@gmail.com'=>'Mr Example'))
+	    ->setTo(array('ahm.rizawan@gmail.com'=>'Mr Example'))
+	    ->addPart('My Plain Text Message','text/plain')
+	    ->setBody('<p>My HTML Message</p>','text/html');
+
+	// Send the email
+	$mailer->send($message);
+	*/
+
+
+
+	$transporter = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+                ->setUsername('ahm.rizawan@gmail.com')
+                ->setPassword('strongp455w0rd');
+
+    $mailer = Swift_Mailer::newInstance($transporter);
+
+	// To use the ArrayLogger
+   	$logger = new Swift_Plugins_Loggers_ArrayLogger();
+   	$mailer->registerPlugin(new Swift_Plugins_LoggerPlugin($logger));
+
+	$message = Swift_Message::newInstance('Password Recovery')
+	                        ->setFrom(array('info@example.com' => 'example.net'))
+	                        ->setTo(array('ahm.rizawan@gmail.com' => 'Mr. Test'))
+	                        ->addPart('Password: 222', 'text/plain')
+	                        ->setBody('My HTML Message', 'text/html');
+
+	$numSent = $mailer->send($message, $failures);
+	if ($numSent <1 ) dd($logger->dump()); //see logs
+
+});
 /*
 |--------------------------------------------------------------------------
 | Application 404 & 500 Error Handlers
@@ -36,9 +79,17 @@ Event::listen('500', function($exception)
 {
 	return Response::error('500');
 });
+/*
+|--------------------------------------------------------------------------
+| Application Filters
+|--------------------------------------------------------------------------
+*/
 
-
-Route::filter('sentry', function()
+Route::filter('corelogin', function()
 {
-	if (!Sentry::check()) return Redirect::to('login');
+    if (!Sentry::check()) return Redirect::to('/admin/login');
+});
+Route::filter('alreadycoreloggedin', function()
+{
+    if (Sentry::check()) return Redirect::to('/admin');
 });
